0abd6f2b317d6a34c0151d9fb1ac8d9fa7f6b8c3
Maryam Tahir <128903308+maryamtahirexe@users.noreply.github.com>
2025-03-22 21:56:09 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://98.81.143.34:3000",
     credentials: true,
   })
 );
