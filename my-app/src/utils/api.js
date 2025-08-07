569a857fce530bba6c1b3c364c1e2f0c23227e9d
maryamtahirexe <maryamtahir061@gmail.com>
2025-08-07 08:52:04 +0500

    newwww

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://webprojectbackend-production-ef2c.up.railway.app',
+  baseURL: 'https://webprojectbackend-production-ef2c.up.railway.app',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
